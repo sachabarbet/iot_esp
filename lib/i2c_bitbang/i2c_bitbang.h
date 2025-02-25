e34edfbc99f5f6f3112f47dda8459511a88aafcf
AurelienFerrari <112978012+AurelienFerrari@users.noreply.github.com>
2025-02-25 12:26:47 +0100

    Update i2c_bitbang.h

--- lib/i2c_bitbang/i2c_bitbang.h
+++ lib/i2c_bitbang/i2c_bitbang.h
@@ -3,12 +3,11 @@
 
 #include <Arduino.h>
 
-// Définition des broches I2C manuelles
 #define SDA_PIN 21
 #define SCL_PIN 22
 
-#define BH1750_ADDR 0x23  // Adresse I2C du capteur (0x5C possible)
-#define BH1750_CMD_HRES 0x10  // Mode Haute Résolution (1 lux)
+#define BH1750_ADDR 0x23  
+#define BH1750_CMD_HRES 0x10
 
 void i2c_init();
 void i2c_start();
