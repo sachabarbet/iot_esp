6deb8e6e46d391cbdba3fb3ea754016c36313715
AurelienFerrari <112978012+AurelienFerrari@users.noreply.github.com>
2025-02-25 12:26:30 +0100

    Update i2c_bitbang.cpp

--- lib/i2c_bitbang/i2c_bitbang.cpp
+++ lib/i2c_bitbang/i2c_bitbang.cpp
@@ -1,9 +1,7 @@
 #include "i2c_bitbang.h"
 
-// Petite pause pour la synchronisation I2C
 #define I2C_DELAY() delayMicroseconds(4)
 
-// Fonction pour configurer l'I2C en mode inactif
 void i2c_init() {
     pinMode(SDA_PIN, OUTPUT);
     pinMode(SCL_PIN, OUTPUT);
@@ -11,14 +9,12 @@ void i2c_init() {
     digitalWrite(SCL_PIN, HIGH);
 }
 
-// Générer une condition de Start I2C
 void i2c_start() {
     digitalWrite(SDA_PIN, LOW);
     I2C_DELAY();
     digitalWrite(SCL_PIN, LOW);
 }
 
-// Générer une condition de Stop I2C
 void i2c_stop() {
     digitalWrite(SDA_PIN, LOW);
     digitalWrite(SCL_PIN, HIGH);
@@ -26,7 +22,6 @@ void i2c_stop() {
     digitalWrite(SDA_PIN, HIGH);
 }
 
-// Écrire un octet sur le bus I2C
 bool i2c_write(uint8_t data) {
     for (uint8_t i = 0; i < 8; i++) {
         digitalWrite(SDA_PIN, (data & 0x80) ? HIGH : LOW);
@@ -37,7 +32,6 @@ bool i2c_write(uint8_t data) {
         digitalWrite(SCL_PIN, LOW);
     }
 
-    // Lecture de l'ACK du capteur
     pinMode(SDA_PIN, INPUT);
     digitalWrite(SCL_PIN, HIGH);
     I2C_DELAY();
@@ -47,7 +41,6 @@ bool i2c_write(uint8_t data) {
     return ack;
 }
 
-// Lire un octet du bus I2C avec ACK/NACK
 uint8_t i2c_read(bool ack) {
     uint8_t data = 0;
     pinMode(SDA_PIN, INPUT);
@@ -59,7 +52,6 @@ uint8_t i2c_read(bool ack) {
         digitalWrite(SCL_PIN, LOW);
     }
 
-    // Envoyer ACK/NACK
     pinMode(SDA_PIN, OUTPUT);
     digitalWrite(SDA_PIN, ack ? LOW : HIGH);
     digitalWrite(SCL_PIN, HIGH);
@@ -69,7 +61,6 @@ uint8_t i2c_read(bool ack) {
     return data;
 }
 
-// Lire la luminosité depuis le BH1750
 uint16_t readBH1750() {
     i2c_start();
     if (!i2c_write(BH1750_ADDR << 1) || !i2c_write(BH1750_CMD_HRES)) {
@@ -79,9 +70,8 @@ uint16_t readBH1750() {
     }
     i2c_stop();
 
-    delay(180);  // Temps de mesure du capteur
+    delay(180);  
 
-    // Lecture des 2 octets
     i2c_start();
     if (!i2c_write((BH1750_ADDR << 1) | 1)) {
         i2c_stop();
@@ -92,5 +82,5 @@ uint16_t readBH1750() {
     uint8_t lsb = i2c_read(false);
     i2c_stop();
 
-    return ((msb << 8) | lsb) / 1.2;  // Conversion en lux
+    return ((msb << 8) | lsb) / 1.2;
 }
