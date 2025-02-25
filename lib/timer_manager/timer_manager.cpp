8789fb994cfeded1e83d0457c81a99637814faf8
AurelienFerrari <112978012+AurelienFerrari@users.noreply.github.com>
2025-02-25 12:27:49 +0100

    Update timer_manager.cpp

--- lib/timer_manager/timer_manager.cpp
+++ lib/timer_manager/timer_manager.cpp
@@ -1,36 +1,30 @@
 #include "timer_manager.h"
 
-// Constructeur : Initialise le compteur et le seuil
 Timer::Timer(unsigned long thresholdValue) {
     threshold = thresholdValue;
     counter = 0;
 }
 
-// Vérifie si le timer a atteint son seuil
 bool Timer::isExpired() {
     if (counter >= threshold) {
-        counter = 0; // Réinitialisation après expiration
+        counter = 0; 
         return true;
     }
     return false;
 }
 
-// Réinitialise le compteur
 void Timer::reset() {
     counter = 0;
 }
 
-// Incrémente le compteur
 void Timer::increment() {
     counter++;
 }
 
-// Récupère la valeur du compteur
 unsigned long Timer::getCounter() const {
     return counter;
 }
 
-// Récupère la valeur du seuil de mise à jour
 unsigned long Timer::getThreshold() const {
     return threshold;
 }
