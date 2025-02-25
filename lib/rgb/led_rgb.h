5462e48daafa4c0694db86425e2b1e3a04cfa848
AurelienFerrari <112978012+AurelienFerrari@users.noreply.github.com>
2025-02-25 12:27:27 +0100

    Update led_rgb.h

--- lib/rgb/led_rgb.h
+++ lib/rgb/led_rgb.h
@@ -3,31 +3,26 @@
 
 #include <Arduino.h>
 #include "timer_manager.h"
-#include "i2c_bitbang.h" // Pour lire la luminosité
+#include "i2c_bitbang.h" 
 
-// Définition des broches de la LED RGB
 #define RED_PIN 32
 #define GREEN_PIN 33
 #define BLUE_PIN 25
 
-// Seuil de luminosité pour allumer la LED
 #define LIGHT_THRESHOLD 200
 
-// États possibles de la LED
 enum LedState {
     LED_OFF,
     LED_ON
 };
 
-// Structure pour gérer une LED avec un Timer basé sur un compteur
 struct Led {
     LedState state;
-    Timer timer;  // Timer pour la mise à jour de l’état
+    Timer timer;  
 
-    Led(unsigned long threshold = 500);  // Constructeur
+    Led(unsigned long threshold = 500);  
 };
 
-// Déclarations des fonctions
 void led_init();
 void setRgbLedColor(int red, int green, int blue);
 void setLedState(Led &led, LedState newState);
